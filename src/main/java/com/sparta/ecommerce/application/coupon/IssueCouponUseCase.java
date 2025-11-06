8bc1e5780c3bf5a8ccb473ef72b6aff59d113136
gyunam <gyunam@vivasoft.co.kr>
2025-11-06 20:09:30 +0900

    feat: ConcurrentHashMap을 사용해서 선착순 쿠폰 발급 동시성 적용

--- src/main/java/com/sparta/ecommerce/application/coupon/IssueCouponUseCase.java
+++ src/main/java/com/sparta/ecommerce/application/coupon/IssueCouponUseCase.java
@@ -7,12 +7,23 @@ import com.sparta.ecommerce.domain.coupon.exception.CouponException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 @Service
 @RequiredArgsConstructor
 public class IssueCouponUseCase {
     private final UserService userService;
     private final CouponService couponService;
     private final UserCouponService userCouponService;
+
+    // 쿠폰Id Lock 관리
+    private final ConcurrentHashMap<Long, Lock> couponLocks = new ConcurrentHashMap<>();
+
+    private Lock getCouponLock(long couponId) {
+        return couponLocks.computeIfAbsent(couponId, id -> new ReentrantLock());
+    }
 
     /**
      * 쿠폰 발급
@@ -28,25 +39,32 @@ public class IssueCouponUseCase {
      * @param couponId 쿠폰 ID
      */
     public void issueCoupon(Long userId, Long couponId) {
-        // 1. 사용자 존재 여부 확인
-        userService.getUserById(userId);
+        Lock lock = getCouponLock(couponId);
+        lock.lock();
 
-        // 2. 쿠폰 조회
-        Coupon coupon = couponService.getCoupon(couponId);
+        try {
+            // 1. 사용자 존재 여부 확인
+            userService.getUserById(userId);
 
-        // 3. 쿠폰 발급 가능 여부 검증 (만료일 + 재고)
-        coupon.validateIssuable();
+            // 2. 쿠폰 조회
+            Coupon coupon = couponService.getCoupon(couponId);
 
-        // 4. 중복 발급 검증
-        if (userCouponService.hasCoupon(userId, couponId)) {
-            throw new CouponException(CouponErrorCode.COUPON_ALREADY_ISSUED);
+            // 3. 쿠폰 발급 가능 여부 검증 (만료일 + 재고)
+            coupon.validateIssuable();
+
+            // 4. 중복 발급 검증
+            if (userCouponService.hasCoupon(userId, couponId)) {
+                throw new CouponException(CouponErrorCode.COUPON_ALREADY_ISSUED);
+            }
+
+            // 5. 쿠폰 발급 수량 증가
+            coupon.increaseIssuedQuantity();
+            couponService.saveCoupon(coupon);
+
+            // 6. 사용자에게 쿠폰 발급
+            userCouponService.issueCoupon(userId, couponId);
+        } finally {
+            lock.unlock();
         }
-
-        // 5. 쿠폰 발급 수량 증가
-        coupon.increaseIssuedQuantity();
-        couponService.saveCoupon(coupon);
-
-        // 6. 사용자에게 쿠폰 발급
-        userCouponService.issueCoupon(userId, couponId);
     }
 }
